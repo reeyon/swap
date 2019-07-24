8946a376ff2587f9eddf41ffb7b92d7b60f65976
sebseb7 <sebgreenbus@gmail.com>
2019-07-24 16:48:39 +0200

    precomputed hashes

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -4745,7 +4745,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "cfca50ea0c87718ac92a14654c60d7ee8f6453e2765b329b40d10da4ed85a4f2";
+static const char expected_block_hashes_hash[] = "907182f7b9ce6b11d48b23f102b24c74d1d5e5bb819fab68890351a10947128b";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
