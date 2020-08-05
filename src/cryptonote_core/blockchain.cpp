16ea3c5024771bf737fbb767c1ef9daa023d4475
reeyon <reeyonlim@gmail.com>
2020-08-05 19:32:09 +0800

    checkpoints up to height 3615906

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5037,7 +5037,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "37e15136d7527e47940ef85bff9d258b940c583bcc2e820aa9a98833a7344ece";
+static const char expected_block_hashes_hash[] = "b3e4582eacf44e00a74e0598ae851ae2e10ba1351e27294c4af08011b4e1ddd6";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
